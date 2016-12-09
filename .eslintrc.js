b5ddd53fd0b001cb7e510831dcb1dd5473f10e92
Thang Trinh <thangtrinh@MacBook-Pro.local>
2016-12-09 17:26:20 +0700

    reconfig eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "no-console": [0]
-    }
+  "extends": "eslint-config-airbnb",
+  "ecmaFeatures": {
+    globalReturn: true
+  },
+  "parser": "babel-eslint",
+  "rules": {
+    "no-console": [0],
+    "max-len": [1, 120, 2, {ignoreComments: true}],
+    "no-else-return": [0],
+    "semi": [2, "never"],
+    "import/prefer-default-export": [0],
+    "arrow-body-style": [0],
+  },
+  "globals": {
+    "__CLIENT__": true,
+    "__SERVER__": true,
+    "__PRODUCTION__": true,
+    "__DEV__": true,
+    "document": false,
+    "escape": false,
+    "navigator": false,
+    "unescape": false,
+    "window": false,
+    "describe": true,
+    "before": true,
+    "after": true,
+    "it": true,
+  }
 };
